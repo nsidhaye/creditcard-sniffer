a801f93628682161906eab0a4a16615ab0600c32
Nikhil Sidhaye <Nikhil Sidhaye>
2016-04-14 15:49:02 -0600

    Remove unwanted snipett

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 var pattern = require("./pattern.js")
-var debug = require('debug')('myApp')
+var debug = require('debug')('myApp:index')
 
-// RegEx()
 
 var testString = "4454365654654365dfssasdas"
 
 var testString1 = "s dfsdf ksd fks \nfksj4454365654654365dfssasdas\ndfgd4454-3656-5465-4365gczxd"
 
-function handleResult(err, result) {
-    if (err) {
-        // Just an example. You may want to do something with the error.
-        console.error(err.stack || err.message);
-
-        // You should return in this branch, since there is no result to use
-        // later and that could cause an exception.
-        return;
-    }
-
-    // All your logic with the result.
-    return result
-}
 
 console.log("All Match: " + testString1.match(pattern.RegEx()))
-
-// console.log(testString1.match(pattern.RegEx(handleResult)))
-
 
 var result = pattern.findMatches(null,pattern.RegEx(),testString1)
 
